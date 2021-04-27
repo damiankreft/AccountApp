fc5a8655ebc3332fe5d1730ec025f1315687806d
Damian Kreft <damiankreft87@gmail.com>
2021-04-27 22:00:19 +0200

    Delete auto-generated files.

--- AccountApp.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AccountApp.Core
-{
-    public class Class1
-    {
-    }
-}
--- AccountApp.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AccountApp.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
