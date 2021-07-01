b35aeae27c207f313e0b0e7e03ca0eca7bc4c1e6
Damian Kreft <dystopiannightrunner@gmail.com>
2021-07-01 21:00:24 +0200

    Add tests.

--- AccountApp.Tests/Services/AccountServiceTests.cs
+++ AccountApp.Tests/Services/AccountServiceTests.cs
@@ -14,49 +14,42 @@ namespace AccountApp.Tests.Services
     [TestFixture]
     public class AccountServiceTests
     {
-        [Test]
-        public async Task indicates_test_framework_success_when_indicated()
-        {
-            var repository = new Mock<IAccountRepository>();
-            var mapper = new Mock<AutoMapper.IMapper>();
+        private Mock<IAccountRepository> _repository;
+        private Mock<AutoMapper.IMapper> _mapper;
 
-            var accountService = new AccountService(repository.Object, mapper.Object);
-            await accountService.RegisterAsync("testowyEmail@dot.com", "testowyUser", "testoweHaslo");
-            
-            repository.Verify(x => x.AddAsync(It.IsAny<Account>()), Times.Once);
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new Mock<IAccountRepository>();
+            _mapper = new Mock<AutoMapper.IMapper>();
         }
 
         [Test]
-        public async Task returns_account_with_given_email_if_account_exists()
+        public async Task indicates_test_framework_success_when_indicated()
         {
-            var repository = new InMemoryAccountRepository();
-            var mapper = new Mock<AutoMapper.IMapper>();
-            var accountService = new AccountService(repository, mapper.Object);
-
-            var account = await accountService.GetAsync("test1@example.com");
-            Assert.IsInstanceOf(typeof(AccountDto), account);
+            var accountService = new AccountService(_repository.Object, _mapper.Object);
+            await accountService.RegisterAsync("testowyEmail@dot.com", "testowyUser", "testoweHaslo");
+            
+            _repository.Verify(x => x.AddAsync(It.IsAny<Account>()), Times.Once);
         }
 
         [Test]
         public async Task gets_all_accounts()
         {
-            var repositoryMock = new Mock<IAccountRepository>();
-            var mapperMock = new Mock<AutoMapper.IMapper>();
-
-            var accountService = new AccountService(repositoryMock.Object, mapperMock.Object);
+            var accountService = new AccountService(_repository.Object, _mapper.Object);
             await accountService.GetAsync("email@example.com");
 
             var account = new Account("email@example.com", "username", "passwd");
-            repositoryMock.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(() => null);
+            _repository.Setup(x => x.GetAsync(It.IsAny<string>())).ReturnsAsync(() => null);
 
-            repositoryMock.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once);
+            _repository.Verify(x => x.GetAsync(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public async Task returns()
         {
-            var repositoryMock = new Mock<IAccountRepository>();
-            var mapperMock = new Mock<AutoMapper.IMapper>();
+            var repositoryMock = _repository;
+            var mapperMock = _mapper;
 
             var accountService = new AccountService(repositoryMock.Object, mapperMock.Object);
             await accountService.GetAsync("email@example.com");
