dda184df226fbaf0ff2630f44d0305baaf0118dc
Damian Kreft <damiankreft87@gmail.com>
2021-08-26 00:54:49 +0200

    Remove sensitive data (password, salt) from AccountDto, add Id, Username and LastAttemptIp.

--- src/AccountApp.Infrastructure/Dto/AccountDto.cs
+++ src/AccountApp.Infrastructure/Dto/AccountDto.cs
@@ -2,19 +2,21 @@ namespace AccountApp.Infrastructure.Dto
 {
     public class AccountDto
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
-        public string Salt { get; set; }
         public string Role { get; init; }
+        public string LastAttemptIp { get; set; }
 
         public AccountDto() { }
 
-        public AccountDto(string email, string password, string salt, string role)
+        public AccountDto(string email, string role, int id, string username, string lastAttemptIp)
         {
             Email = email;
-            Password = password;
-            Salt = salt;
             Role = role;
+            Id = id;
+            Username = username;
+            LastAttemptIp = lastAttemptIp;
         }
     }
 }
