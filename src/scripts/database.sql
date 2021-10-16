4fad434aad96943bf8d16275967f79c7a7b084b0
Damian Kreft <damiankreft87@gmail.com>
2021-10-16 12:15:33 +0200

    Update database script.

--- src/scripts/database.sql
+++ src/scripts/database.sql
@@ -1,23 +1,20 @@
-CREATE DATABASE AccountApp
+CREATE DATABASE AccountApp;
 
-USE AccountApp
+USE AccountApp;
 
 CREATE TABLE Accounts (
-    Id NUMERIC PRIMARY KEY NOT NULL,
-    Email NVARCHAR(100) NOT NULL,
+    Id UNIQUEIDENTIFIER PRIMARY KEY NOT NULL,
+    Username NVARCHAR(100) NOT NULL,
     Password NVARCHAR(200) NOT NULL,
     Salt NVARCHAR(200) NOT NULL,
-    Username NVARCHAR(100) NOT NULL,
-    FullName NVARCHAR(100),
     Role NVARCHAR(10),
-    -- CreatedAt DATETIME NOT NULL,
-    -- UpdatedAt DATETIME NOT NULL,
+    Email NVARCHAR(100) NOT NULL,
+    RegistrationEmail NVARCHAR(100),
+    CreatedAt DATETIME NOT NULL,
+    UpdatedAt DATETIME,
     LastAttemptIp NVARCHAR(46),
     LastIp NVARCHAR(46),
-    Os NVARCHAR(30),
-    RegistrationEmail NVARCHAR(100)
-)
+    Os NVARCHAR(30)
+);
 
-SELECT * FROM Accounts
-
-DROP TABLE Accounts
+SELECT * FROM Accounts;
